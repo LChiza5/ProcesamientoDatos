23e974ee140f01ec1fa06ae62064918fc9b2934d
Chiza <lualvarezva@est.utn.ac.cr>
2026-06-16 11:16:55 -0600

    Version 5
    Hicimos varias validaciones en app.js, para la placa, el tipo de vehiculo (que no puede ser otro de carro o moto), las horas y minutos a cobrar (que las horas no pueden ser negativas, igual a los minutos y los minutos no pueden ser mas de 59. Tambien programamos para lo de los cobros y lo probamos en postman

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,6 +23,40 @@ app.get("/",(req,res) => {
     });
 });
 
+app.post("/api/parqueo/calcular", (req,res) =>{
+    const {placa, tipo, horas, minutos} = req.body;
+
+    if (!placa || placa.trim()=="") {
+        res.status(400).json({error:"La placa es obligatoria"})
+    }
+
+    if (tipo!=="carro" && tipo!=="moto" ) {
+        res.status(400).json({error:"El tipo de vehiculo debe ser carro o moto"})
+    }
+
+    if (Number.isNaN(horas) || horas < 0) {
+        res.status(400).json({error:"Las horas no deben ser negativas"})
+    }
+
+    if (Number.isNaN(minutos) || minutos < 0 || minutos > 59 ) {
+        res.status(400).json({error:"Los minutos no deben ser negativos o no deben ser mayor a 59"})
+    }
+
+    const tarifa = tipo==="carro" ? 1200 : 500;
+    
+    let h = Number(horas);
+    let m = Number(minutos);
+    if (m>5) h++;
+
+    res.json({
+        placa: placa,
+        tipo:tipo, 
+        tarifa:tarifa,
+        tiempo: horas,
+        horasCobradas: h
+    });
+});
+
 app.listen(4000, () => {
     console.log(`${NAME} ${VERSION} ejecutandose en http://localhost:4000/`);
 });
