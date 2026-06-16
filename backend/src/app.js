0ba26356dac60b1d4e6821373d3e99a9a3673307
Chiza <lualvarezva@est.utn.ac.cr>
2026-06-16 09:53:10 -0600

    Version 3
    Creamos lo que vendrian siendo las variables de entorno para poderlas visualizar dentro de app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,19 @@
 import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const NAME=process.env.SERVER_NAME;
+const VERSION=process.env.SERVER_VERSION;
+const DESCRIPTION=process.env.SERVER_DESCRIPTION;
+const PORT=process.env.SERVER_PORT;
 
 const app = express();
 
 app.get("/",(req,res) => {
-    res.send("<h1>no se programar</h1>");
+    res.send(`<h1>${NAME} </h1><p>Version: ${VERSION}</p><p>${DESCRIPTION}`);
 });
 
 app.listen(4000, () => {
-    console.log("servidor corriendo");
+    console.log(`${NAME} ${VERSION} ejecutandose en http://localhost:4000/`);
 });
